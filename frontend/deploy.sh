a123c7b7be31ac95557b949a6b2e1d6b0b9ebdeb
Justin Niles <justin.niles12@gmail.com>
2017-05-14 21:53:02 -0700

    Update deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,6 +2,7 @@ ng build
 git add dist
 git commit -m "DEPLOY: Update dist folder"
 cd ..
+git branch -D deploy
 git subtree split --prefix=frontend/dist --branch=deploy
-git push
+git push origin +deploy
 cd frontend
