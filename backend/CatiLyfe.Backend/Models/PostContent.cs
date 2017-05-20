d07c121feddb0868bc579544eb4d6614f1880aaa
petersulucz <peter.sulucz@gmail.com>
2017-05-19 21:48:48 -0700

    Added some markdown magic

--- backend/CatiLyfe.Backend/Models/PostContent.cs
+++ backend/CatiLyfe.Backend/Models/PostContent.cs
@@ -1,5 +1,7 @@
 ﻿namespace CatiLyfe.Backend.Models
 {
+    using HeyRed.MarkdownSharp;
+
     public class PostContent
     {
         public PostContent(DataLayer.Models.PostContent content)
@@ -7,6 +9,25 @@
             this.Index = content.Index;
             this.ContentType = content.ContentType;
             this.Content = content.Content;
+
+            if (this.ContentType == "markdown")
+            {
+                var options = new MarkdownOptions
+                                  {
+                                      AutoHyperlink = true,
+                                      AutoNewLines = true,
+                                      LinkEmails = true,
+                                      QuoteSingleLine = true,
+                                      StrictBoldItalic = true
+                                  };
+
+                var markdown = new Markdown(options);
+                this.Content = markdown.Transform(this.Content);
+            }
+            else
+            {
+                // do nothing
+            }
         }
 
         public int Index { get; }
