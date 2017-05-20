405e2aca6fc8bcc5d0583e6ac940d0343f6b200a
petersulucz <peter.sulucz@gmail.com>
2017-05-20 09:59:12 -0700

    Updated the powershell build script to be the best one

--- frontend/build.ps1
+++ frontend/build.ps1
@@ -1,8 +1,56 @@
-ng build
+param
+(
+    [string]$DeployMessage,
+    [Switch]$Deploy,
+    [switch]$SkipBuild
+)
+
+$currentdirectory = Get-Location
+
+if([System.Io.Path]::GetFileName($currentdirectory.Path) -ne 'frontend')
+{
+    return 'Failure. This directory is wrong. Must be run from "frontend"'
+}
+
+if($Deploy -and -not $DeployMessage)
+{
+    Write-Error 'Deployment message must exist to do a deployment'
+    return
+}
+
+if(-not $SkipBuild)
+{
+    ## Build angular crap
+    Write-Output "Starting ng Build"
+    ng set --global warnings.versionMismatch=false
+    ng build
+}
+else
+{
+    Write-Output 'Skipping build'
+}
+
+## Short circuit here if not deploying
+if(-not $Deploy)
+{
+    return
+}
+
+Write-Output "Beggining production deployment"
+
 git add dist
-git commit -m "DEPLOY: Update dist folder"
-cd ..
-git branch -D deploy
-git subtree split --prefix=frontend/dist --branch=deploy
-git push origin +deploy
-cd frontend
+git commit -m $DeployMessage
+
+# Check if the commit failed
+if($?)
+{
+    Push-Location ..
+    git branch -D deploy > $null
+    git subtree split --prefix=frontend/dist --branch=deploy
+    git push origin +deploy
+    Pop-Location
+}
+else
+{
+    Write-Warning "Nothing deployed. Git doesnt think there are any changes"
+}
