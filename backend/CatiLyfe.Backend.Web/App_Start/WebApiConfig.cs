49459c15072d193b18f0c61267b116bcf44a9583
petersulucz <peter.sulucz@gmail.com>
2017-08-07 23:45:21 -0700

    Making stuff camel case so that Justins stuff stop losing its mind.

--- backend/CatiLyfe.Backend.Web/App_Start/WebApiConfig.cs
+++ backend/CatiLyfe.Backend.Web/App_Start/WebApiConfig.cs
@@ -7,10 +7,17 @@ namespace CatiLyfe.Backend.Web
 {
     using System.Web.Http.Cors;
 
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            //json settings
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+
             // Web API configuration and services
 
             // Enable cors.
