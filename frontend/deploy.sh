5f7bbf478b5acc5e855d8b6758df434b166cbddb
Justin Niles <justin.niles12@gmail.com>
2017-08-16 10:35:25 -0700

    Add intial Frontend Node Server files

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,9 +1,18 @@
+# ng build
+# git add dist
+# git commit -m "DEPLOY: Update dist folder"
+# git push
+
+# TODO: git commit to check if there are any uncommitted or unsynced changes
+echo "Ensure that all changes are committed and pushed to remote"
+
+# TODO: need to persist sessions between logins on vm
+ssh justin@52.191.141.202
+cd catilyfe
+git pull
+echo "Running 'ng build'..."
 ng build
-cp ./web.config ./dist/
-git add dist
-git commit -m "DEPLOY: Update dist folder"
-cd ..
-git branch -D deploy
-git subtree split --prefix=frontend/dist --branch=deploy
-git push origin +deploy
-cd frontend
+echo "Copying dist to server folder..."
+cp -r ./dist/* ./server/public
+# Don't need to restart node server, since we're just serving static content??
+exit
