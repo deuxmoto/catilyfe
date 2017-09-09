c2431bb2597658145af8bcce0db7ffd4061e5a6b
Justin Niles <justin.niles12@gmail.com>
2017-09-08 22:52:33 -0700

    Update deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,4 +1,5 @@
 # TODO: git commit to check if there are any uncommitted or unsynced changes
 # echo "Ensure that all changes are committed and pushed to remote"
 
-ssh justin@52.191.141.202 "cd ~/catilyfe/frontend/server; ./update-server.sh;"
+echo "Remoting into server, run ./update-server.sh to update..."
+ssh -t justin@52.191.141.202 "cd ~/catilyfe/frontend/server; bash;"
