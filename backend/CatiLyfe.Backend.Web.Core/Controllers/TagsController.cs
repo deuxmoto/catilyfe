05e1018632206baad827d156f6d729a9cd521954
Peter Sulucz <peter.sulucz@gmail.com>
2017-12-15 01:27:16 -0500

    Tags api

--- backend/CatiLyfe.Backend.Web.Core/Controllers/TagsController.cs
+++ backend/CatiLyfe.Backend.Web.Core/Controllers/TagsController.cs
@@ -1,8 +1,13 @@
-﻿using CatiLyfe.DataLayer;
+﻿using CatiLyfe.Backend.Web.Models.Tags;
+using CatiLyfe.DataLayer;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CatiLyfe.Backend.Web.Core.Controllers
 {
+    [Route("/[controller]")]
     public class TagsController : Controller
     {
         private readonly ICatiDataLayer dataLayer;
@@ -12,6 +17,11 @@ namespace CatiLyfe.Backend.Web.Core.Controllers
             this.dataLayer = datalayer;
         }
 
-
+        [HttpGet]
+        public async Task<IEnumerable<TagInfoModel>> GetTags()
+        {
+            var tags = await this.dataLayer.GetTags();
+            return tags.Select(t => new TagInfoModel(t.Tag, t.Weight));
+        }
     }
 }
---
+++ backend/CatiLyfe.Backend.Web.Models/Tags/TagInfoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatiLyfe.Backend.Web.Models.Tags
+{
+    public class TagInfoModel
+    {
+        public TagInfoModel(string tag, int weight)
+        {
+            this.Tag = tag;
+            this.Weight = weight;
+        }
+
+        public string Tag { get; private set; }
+
+        public int Weight { get; private set; }
+    }
+}
